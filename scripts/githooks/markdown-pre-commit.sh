2df24fc6dd6fa10e90b6f72840646b0255cd31b1
Tim Ireland <tim.ireland@nhs.net>
2023-05-05 16:29:31 +0100

    Added workflow for MARKDOWN text

--- scripts/githooks/markdown-pre-commit.sh
+++ scripts/githooks/markdown-pre-commit.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Check Markdown formating of all the "*.md" files that are changed and commited to the current branch.
+# Pre-commit git hook to check the Markdown rules complience over changed files
 #
 # Usage:
-#   $ [options] ./markdown-check-format.sh
+#   $ ./markdown-pre-commit.sh
 #
 # Options:
-#   BRANCH_NAME=other-branch-than-main  # Branch to compare with
+#   BRANCH_NAME=other-branch-than-main  # Branch to compare with
+#
+# Notes:
+#   Please, make sure to enable Markdown linting in your IDE. For the Visual Studio Code editor it is
+#   `davidanson.vscode-markdownlint` that is already specified in the `.vscode/extensions.json` file.
 
-# Please, make sure to enable Markdown linting in your IDE. For the Visual Studio Code editor it is
-# `davidanson.vscode-markdownlint` that is already specified in the `.vscode/extensions.json` file.
+# ==============================================================================
 
 files=$((git diff --diff-filter=ACMRT --name-only origin/${BRANCH_NAME:-main}.. "*.md"; git diff --name-only "*.md") | sort | uniq)
 if [ -n "$files" ]; then
