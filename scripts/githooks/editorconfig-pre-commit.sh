c043a5e748cd59178403621519afc8e0722bf51e
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2023-05-14 20:37:49 +0100

    Refactor file and markdown format scripts

--- scripts/githooks/editorconfig-pre-commit.sh
+++ scripts/githooks/editorconfig-pre-commit.sh
@@ -1,32 +1,78 @@
 #!/bin/bash
 
-# Pre-commit git hook to check the EditorConfig rules complience over changed files
+set +e
+
+# Pre-commit git hook to check the EditorConfig rules complience over changed
+# files.
 #
 # Usage:
 #   $ ./editorconfig-pre-commit.sh
 #
+# Options:
+#   BRANCH_NAME=other-branch-than-main  # Branch to compare with, default is `origin/main`
+#   ALL_FILES=true                      # Check all files, default is `false`
+#   VERBOSE=true                        # Show all the executed commands, default is `false`
+#
 # Exit codes:
-#   0 - all files are formatted correctly
-#   1 - files are not formatted correctly
+#   0 - All files are formatted correctly
+#   1 - Files are not formatted correctly
 #
 # Notes:
-#   Due to the file name escaping issue files are checked one by one.
+#   1) Please, make sure to enable EditorConfig linting in your IDE. For the
+#   Visual Studio Code editor it is `editorconfig.editorconfig` that is already
+#   specified in the `./.vscode/extensions.json` file.
+#   2) Due to the file name escaping issue files are checked one by one.
 
 # ==============================================================================
 
 exit_code=0
 image_digest=0f8f8dd4f393d29755bef2aef4391d37c34e358d676e9d66ce195359a9c72ef3 # 2.7.0
-changed_files=$(git diff --name-only --diff-filter=ACMRT)
 
-while read file; do
+# ==============================================================================
+
+function main() {
+
+  if is_arg_true "$ALL_FILES"; then
+
+    # Check all files
     docker run --rm --platform linux/amd64 \
-        --volume=$PWD:/check \
-        mstruebing/editorconfig-checker@sha256:$image_digest \
+      --volume=$PWD:/check \
+      mstruebing/editorconfig-checker@sha256:$image_digest \
+        ec \
+          --exclude '.git/'
+
+  else
+
+    # Check changed files only
+    changed_files=$(git diff --diff-filter=ACMRT --name-only ${BRANCH_NAME:-origin/main})
+    if [ -n "$changed_files" ]; then
+      while read file; do
+        docker run --rm --platform linux/amd64 \
+          --volume=$PWD:/check \
+          mstruebing/editorconfig-checker@sha256:$image_digest \
             ec \
-                --exclude '.git/' \
-                "$file"
-    [ $? != 0 ] && exit_code=1 ||:
-done < <(echo "$changed_files")
-[ $? != 0 ] && exit_code=1 ||:
+              --exclude '.git/' \
+              "$file"
+        [ $? != 0 ] && exit_code=1 ||:
+      done < <(echo "$changed_files")
+    fi
+
+  fi
+}
+
+function is_arg_true() {
+
+  if [[ "$1" =~ ^(true|yes|y|on|1|TRUE|YES|Y|ON)$ ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# ==============================================================================
+
+is_arg_true "$VERBOSE" && set -x
+
+main $*
 
 exit $exit_code
--- scripts/githooks/markdown-pre-commit.sh
+++ scripts/githooks/markdown-pre-commit.sh
@@ -1,27 +1,74 @@
 #!/bin/bash
 
-# Pre-commit git hook to check the Markdown rules complience over changed files
+set -e
+
+# Pre-commit git hook to check the Markdown rules complience over changed
+# files.
 #
 # Usage:
 #   $ ./markdown-pre-commit.sh
 #
 # Options:
-#   BRANCH_NAME=other-branch-than-main  # Branch to compare with
+#   BRANCH_NAME=other-branch-than-main  # Branch to compare with, default is `origin/main`
+#   ALL_FILES=true                      # Check all files, default is `false`
+#   VERBOSE=true                        # Show all the executed commands, default is `false`
+#
+# Exit codes:
+#   0 - All files are formatted correctly
+#   1 - Files are not formatted correctly
 #
 # Notes:
-#   Please, make sure to enable Markdown linting in your IDE. For the Visual Studio Code editor it is
-#   `davidanson.vscode-markdownlint` that is already specified in the `.vscode/extensions.json` file.
+#   1) Please, make sure to enable Markdown linting in your IDE. For the Visual
+#   Studio Code editor it is `davidanson.vscode-markdownlint` that is already
+#   specified in the `./.vscode/extensions.json` file.
+#   2) To see the full list of the rules, please visit
+#   https://github.com/DavidAnson/markdownlint/blob/main/doc/Rules.md
 
 # ==============================================================================
 
 image_digest=3e42db866de0fc813f74450f1065eab9066607fed34eb119d0db6f4e640e6b8d # v0.34.0
 
-files=$((git diff --diff-filter=ACMRT --name-only origin/${BRANCH_NAME:-main}.. "*.md"; git diff --name-only "*.md") | sort | uniq)
-if [ -n "$files" ]; then
-  image=ghcr.io/igorshubovych/markdownlint-cli@sha256:$image_digest
-  docker run --rm \
-    -v $PWD:/workdir \
-    $image \
-      $files \
-      --disable MD013 MD033
-fi
+# ==============================================================================
+
+function main() {
+
+  if is_arg_true "$ALL_FILES"; then
+
+    # Check all files
+    docker run --rm --platform linux/amd64 \
+      --volume=$PWD:/workdir \
+      ghcr.io/igorshubovych/markdownlint-cli@sha256:$image_digest \
+        "*.md" \
+        --disable MD013 MD033
+
+  else
+
+    # Check changed files only
+    changed_files=$(git diff --diff-filter=ACMRT --name-only ${BRANCH_NAME:-origin/main} "*.md")
+    if [ -n "$changed_files" ]; then
+      docker run --rm --platform linux/amd64 \
+        --volume=$PWD:/workdir \
+        ghcr.io/igorshubovych/markdownlint-cli@sha256:$image_digest \
+          $changed_files \
+          --disable MD013 MD033
+    fi
+
+  fi
+}
+
+function is_arg_true() {
+
+  if [[ "$1" =~ ^(true|yes|y|on|1|TRUE|YES|Y|ON)$ ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# ==============================================================================
+
+is_arg_true "$VERBOSE" && set -x
+
+main $*
+
+exit 0
