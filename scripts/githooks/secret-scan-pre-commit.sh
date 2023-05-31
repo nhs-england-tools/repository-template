59ea13c2a98fec50c8f78c0d48871d97446e186d
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2023-05-31 22:19:03 +0100

    Refactor

--- scripts/githooks/secret-scan-pre-commit.sh
+++ scripts/githooks/secret-scan-pre-commit.sh
@@ -8,7 +8,7 @@ set -e
 #   $ ./secret-scan-pre-commit.sh
 #
 # Options:
-#   ALL_FILES=true  # Check all files, default is `false`
+#   ALL_FILES=true  # Scan whole git history or 'last-commit', default is `false`
 #   VERBOSE=true    # Show all the executed commands, default is `false`
 #
 # Exit codes:
@@ -18,29 +18,28 @@ set -e
 
 # ==============================================================================
 
-image_version=v8.16.3@sha256:05b48ff3f4fd7daa9487b42cbf9d576f2dc0dbe2551e3d0a8738e18ba2278091 # v8.16.3
+image_version=v8.16.3@sha256:05b48ff3f4fd7daa9487b42cbf9d576f2dc0dbe2551e3d0a8738e18ba2278091
 
 # ==============================================================================
 
 function main() {
 
   if is_arg_true "$ALL_FILES"; then
-
-    # Check all files
-    docker run --rm --platform linux/amd64 \
-      --volume=$PWD:/check \
-      ghcr.io/gitleaks/gitleaks:$image_version \
-        detect --source=/check --verbose --redact
-
+    # Scan whole git history
+    cmd="detect --source=/scan --verbose --redact"
+  elif [ "$ALL_FILES" == "last-commit" ]; then
+    # Scan the last commit
+    cmd="detect --source=/scan --verbose --redact --log-opts=-1"
   else
-
-    # Check changed files only
-    docker run --rm --platform linux/amd64 \
-      --volume=$PWD:/check \
-      ghcr.io/gitleaks/gitleaks:$image_version \
-        protect --source=/check --verbose --redact --staged
-
+    # Scan staged files only
+    cmd="protect --source=/scan --verbose --staged"
   fi
+
+  docker run --rm --platform linux/amd64 \
+    --volume=$PWD:/scan \
+    --workdir=/scan \
+    ghcr.io/gitleaks/gitleaks:$image_version \
+      $cmd
 }
 
 function is_arg_true() {
