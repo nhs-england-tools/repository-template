cfff6b11b632a9e8bb7dcdcee36fc5a80ed0f943
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2023-05-31 14:51:51 +0100

    Add secret scanning
    Co-authored-by: Jonathan Pearce <j.pearce1@nhs.net>
    Co-authored-by: Tamara Goldschmidt <tamaragoldschmidt@users.noreply.github.com>

---
+++ scripts/githooks/secret-scan-pre-commit.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set +e
+
+# Pre-commit git hook to check the EditorConfig rules compliance over changed
+# files. It ensures all non-binary files across the codebase are formatted
+# according to the style defined in the `.editorconfig` file.
+#
+# Usage:
+#   $ ./editorconfig-pre-commit.sh
+#
+# Options:
+#   BRANCH_NAME=other-branch-than-main  # Branch to compare with, default is `origin/main`
+#   ALL_FILES=true                      # Check all files, default is `false`
+#   VERBOSE=true                        # Show all the executed commands, default is `false`
+#
+# Exit codes:
+#   0 - All files are formatted correctly
+#   1 - Files are not formatted correctly
+#
+# Notes:
+#   1) Please, make sure to enable EditorConfig linting in your IDE. For the
+#   Visual Studio Code editor it is `editorconfig.editorconfig` that is already
+#   specified in the `./.vscode/extensions.json` file.
+#   2) Due to the file name escaping issue files are checked one by one.
+
+# ==============================================================================
+
+exit_code=0
+image_version=v8.16.3@sha256:05b48ff3f4fd7daa9487b42cbf9d576f2dc0dbe2551e3d0a8738e18ba2278091 # v8.16.3
+
+# ==============================================================================
+
+function main() {
+
+  if is_arg_true "$ALL_FILES"; then
+
+    # Check all files
+    docker run --rm --platform linux/amd64 \
+      --volume=$PWD:/check \
+      ghcr.io/gitleaks/gitleaks:$image_version \
+        detect --source=/check --verbose --redact
+
+  else
+
+    # Check changed files only
+    docker run --rm --platform linux/amd64 \
+      --volume=$PWD:/check \
+      ghcr.io/gitleaks/gitleaks:$image_version \
+        protect --source=/check --verbose --redact --staged
+
+  fi
+}
+
+function is_arg_true() {
+
+  if [[ "$1" =~ ^(true|yes|y|on|1|TRUE|YES|Y|ON)$ ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# ==============================================================================
+
+is_arg_true "$VERBOSE" && set -x
+
+main $*
+
+exit $exit_code
