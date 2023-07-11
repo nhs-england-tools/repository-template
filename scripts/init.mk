b8287b77fdc94d3117e99291e015c0b46b7312ea
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2023-07-11 21:09:10 +0100

    Name tagets in .SILENT section and remove '@'

--- scripts/init.mk
+++ scripts/init.mk
@@ -42,10 +42,10 @@ clean:: # Remove all generated and temporary files
 		sbom-spdx*.json
 
 help: # List Makefile targets
-	@awk 'BEGIN {FS = ":.*?# "} /^[ a-zA-Z0-9_-]+:.*? # / {printf "\033[36m%-41s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST) | sort
+	awk 'BEGIN {FS = ":.*?# "} /^[ a-zA-Z0-9_-]+:.*? # / {printf "\033[36m%-41s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST) | sort
 
 list-variables: # List all the variables available to make
-	@$(foreach v, $(sort $(.VARIABLES)),
+	$(foreach v, $(sort $(.VARIABLES)),
 		$(if $(filter-out default automatic, $(origin $v)),
 			$(if $(and $(patsubst %_PASSWORD,,$v), $(patsubst %_PASS,,$v), $(patsubst %_KEY,,$v), $(patsubst %_SECRET,,$v)),
 				$(info $v=$($v) ($(value $v)) [$(flavor $v),$(origin $v)]),
@@ -73,6 +73,8 @@ endif
 	clean \
 	githooks-install \
 	githooks-run \
+	help \
+	list-variables \
 	nodejs-install \
 	python-install \
 	terraform-install
