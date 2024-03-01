7c223b26c8f9bae4b1044474bdca09d42b6c1503
regularfry <alex@blackkettle.org>
2024-03-01 17:25:06 +0000

    First pass at a full terraform example

--- scripts/terraform/examples/terraform-state-aws-s3/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "terraform_state_bucket_name" {
-  description = "The S3 bucket name to store Terraform state"
-  default     = "repository-template-example-terraform-state-store"
-}
-
-variable "terraform_state_table_name" {
-  description = "The DynamoDB table name to acquire Terraform lock"
-  default     = "repository-template-example-terraform-state-lock"
-}
