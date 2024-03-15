182be42112eb5fc614f75d4808d3ce06ff8f2c55
regularfry <alex@blackkettle.org>
2024-03-15 15:18:54 +0000

    Reworked docker docs

--- scripts/docker/docker.mk
+++ scripts/docker/docker.mk
@@ -4,14 +4,29 @@
 # Custom implementation - implementation of a make target should not exceed 5 lines of effective code.
 # In most cases there should be no need to modify the existing make targets.
 
-docker-build: # Build Docker image - optional: docker_dir|dir=[path to the Dockerfile to use, default is '.'] @Development
-	make _docker cmd="build" \
+DOCKER_IMAGE ?= $(or ${docker_image}, $(or ${IMAGE}, $(or ${image}, ghcr.io/org/repo)))
+DOCKER_TITLE ?= $(or "${docker_title}", $(or "${TITLE}", $(or "${title}", "Service Docker image")))
+
+docker-bake-dockerfile: # Create Dockerfile.effective - optional: docker_dir|dir=[path to the image directory where the Dockerfile is located, default is '.'] @Development
+	make _docker cmd="bake-dockerfile" \
 		dir=$(or ${docker_dir}, ${dir})
-	file=$(or ${docker_dir}, ${dir})/Dockerfile.effective
-	scripts/docker/dockerfile-linter.sh
+
+docker-build: # Build Docker image - optional: docker_dir|dir=[path to the Dockerfile to use, default is '.'] @Development
+	dir=$(or ${docker_dir}, ${dir})
+	make _docker cmd="build"
+docker-build: docker-lint
+
+docker-lint: # Run hadolint over the Dockerfile - optional: docker_dir|dir=[path to the image directory where the Dockerfile is located, default is '.'] @Development
+	dir=$(or ${docker_dir}, ${dir})
+	make _docker cmd="lint"
+docker-lint: docker-bake-dockerfile
 
 docker-push: # Push Docker image - optional: docker_dir|dir=[path to the image directory where the Dockerfile is located, default is '.'] @Development
 	make _docker cmd="push" \
+		dir=$(or ${docker_dir}, ${dir})
+
+docker-run: # Run Docker image - optional: docker_dir|dir=[path to the image directory where the Dockerfile is located, default is '.'] @Development
+	make _docker cmd="run" \
 		dir=$(or ${docker_dir}, ${dir})
 
 clean:: # Remove Docker resources (docker) - optional: docker_dir|dir=[path to the image directory where the Dockerfile is located, default is '.'] @Operations
@@ -20,10 +35,8 @@ clean:: # Remove Docker resources (docker) - optional: docker_dir|dir=[path to t
 
 _docker: # Docker command wrapper - mandatory: cmd=[command to execute]; optional: dir=[path to the image directory where the Dockerfile is located, relative to the project's top-level directory, default is '.']
 	# 'DOCKER_IMAGE' and 'DOCKER_TITLE' are passed to the functions as environment variables
-	DOCKER_IMAGE=$(or ${DOCKER_IMAGE}, $(or ${docker_image}, $(or ${IMAGE}, $(or ${image}, ghcr.io/org/repo))))
-	DOCKER_TITLE=$(or "${DOCKER_TITLE}", $(or "${docker_title}", $(or "${TITLE}", $(or "${title}", "Service Docker image"))))
+	dir=$(realpath $(or ${dir}, infrastructure/images/${DOCKER_IMAGE}))
 	source scripts/docker/docker.lib.sh
-	dir=$(realpath ${dir})
 	docker-${cmd} # 'dir' is accessible by the function as environment variable
 
 # ==============================================================================
@@ -40,44 +53,15 @@ docker-shellscript-lint: # Lint all Docker module shell scripts @Quality
 docker-test-suite-run: # Run Docker test suite @ExamplesAndTests
 	scripts/docker/tests/docker.test.sh
 
-docker-example-build: # Build Docker example @ExamplesAndTests
-	source scripts/docker/docker.lib.sh
-	cd scripts/docker/examples/python
-	DOCKER_IMAGE=repository-template/docker-example-python
-	DOCKER_TITLE="Repository Template Docker Python Example"
-	TOOL_VERSIONS="$(shell git rev-parse --show-toplevel)/scripts/docker/examples/python/.tool-versions.example"
-	docker-build
-
-docker-example-lint: # Lint Docker example @ExamplesAndTests
-	dockerfile=scripts/docker/examples/python/Dockerfile
-	file=$${dockerfile} scripts/docker/dockerfile-linter.sh
-
-docker-example-run: # Run Docker example @ExamplesAndTests
-	source scripts/docker/docker.lib.sh
-	cd scripts/docker/examples/python
-	DOCKER_IMAGE=repository-template/docker-example-python
-	args=" \
-		-it \
-		--publish 8000:8000 \
-	"
-	docker-run
-
-docker-example-clean: # Remove Docker example resources @ExamplesAndTests
-	source scripts/docker/docker.lib.sh
-	cd scripts/docker/examples/python
-	DOCKER_IMAGE=repository-template/docker-example-python
-	docker-clean
-
 # ==============================================================================
 
 ${VERBOSE}.SILENT: \
 	_docker \
 	clean \
+	docker-bake-dockerfile \
 	docker-build \
-	docker-example-build \
-	docker-example-clean \
-	docker-example-lint \
-	docker-example-run \
+	docker-lint \
 	docker-push \
+	docker-run \
 	docker-shellscript-lint \
 	docker-test-suite-run \
